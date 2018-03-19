d4f39abebec194162a1ea8911b6af68ebdc43a0e
root <root@localhost.localdomain>
2018-03-19 18:58:54 +0000

    Bugfix

--- docker/build.sh
+++ docker/build.sh
@@ -3,6 +3,15 @@
 docker build -t haufe .
 docker run -d -p 80:80 haufe
 
+echo
+echo
+echo "Visit the http://<IP> and see the web page of the server in docker"
+echo "You should get back something liek tihs"
+echo
+curl http://127.0.0.1/
+echo
+echo
+echo
 
 ### Optionally stop the docker
 
